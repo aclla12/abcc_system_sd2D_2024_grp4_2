d24e29ed5b4c47c754a387e7af02330cc2afcae0
aclla12 <2301163@s.asojuku.ac.jp>
2024-11-11 09:42:05 +0900

    あ
    aa

--- STYLICO/resources/views/homepage.blade.php
+++ STYLICO/resources/views/homepage.blade.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="jan">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -9,7 +9,9 @@
 <h2>STYLICO</h2>
 <h5>Home Page</h5>
 
-<form action = "{{ route('') }}" method = "post">
+<!-- {{ route('') }} -->
+<form action = "#" method = "post">
+@csrf
 <fieldset>
     <input type="text" name = "search" >
 </fieldset>
@@ -17,41 +19,53 @@
 
 <h2>ONLINE STORE</h2>
 
-<form action = "{{ route('') }}" method = "post">@csrf
+<!-- {{ route('') }} -->
+<form action = "#" method = "post">
+@csrf
     <button type = "submit">TOPS</button>
-        <form action = "{{ route('') }}" method = "post">@csrf
+        <!-- {{ route('') }} -->
+        <form action = "#" method = "post">@csrf
             <button type = "submit">OUTER</button>
-                <form action = "{{ route('') }}" method = "post">@csrf
+                <!-- {{ route('') }} -->
+                <form action = "#" method = "post">
+                    @csrf
                     <button type = "submit">SHIRT</button>
                 </form>
         </form>
 </form><br><br>
-
-<form action = "{{ route('') }}" method = "post">@csrf
+<!-- {{ route('') }} -->
+<form action = "#" method = "post">@csrf
     <button type = "submit">BOTTOM</button>
-        <form action = "{{ route('') }}" method = "post">@csrf
+        <!-- {{ route('') }} -->
+        <form action = "#" method = "post">@csrf
             <button type = "submit">SHOES</button>
-                <form action = "{{ route('') }}" method = "post">@csrf
+                <!-- {{ route('') }} -->
+                <form action = "#" method = "post">@csrf
                     <button type = "submit">BAG</button>
                 </form>
         </form>
 </form><br><br>
 
-<form action = "{{ route('') }}" method = "post">@csrf
+<!-- {{ route('') }} -->
+<form action = "#" method = "post">@csrf
     <button type = "submit">JEWWELRY</button>
-        <form action = "{{ route('') }}" method = "post">@csrf
+        <!-- {{ route('') }} -->
+        <form action = "#" method = "post">@csrf
             <button type = "submit">PERFUME</button>
-                <form action = "{{ route('') }}" method = "post">@csrf
+                <!-- {{ route('') }} -->
+                <form action = "#" method = "post">@csrf
                     <button type = "submit">GOODS</button>
                 </form>
         </form>
 </form><br><br>
 
-<form action="{{ route('') }}" method = "post">@csrf
+<!-- {{ route('') }} -->
+<form action="#" method = "post">@csrf
     <p><button type = "submit">MY PAGE☺</button></p>
 </form>
 
-<form action="{{ route('') }}">@csrf
+<!-- {{ route('') }} -->
+<form action="#">@csrf
     <p><button type = "submit">SNAP</button></p>
 </form>
 
@@ -64,7 +78,8 @@
 <h6>・サイトの機能に関するご意見・ご要望</h6>
 <h6>・サービス利用規約</h6>
 
-<form action="{{ route('') }}">@csrf
+<!-- {{ route('') }} -->
+<form action="#">@csrf
     <p><button type = "submit">-LOGOUT-</button></p>
 </form>
 </body>
