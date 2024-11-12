affcbee35703623e442ab5407a5940e2e0c438da
FukuyamaJoichiro <2301193@s.asojuku.ac.jp>
2024-11-12 12:45:23 +0900

    payment 作成
    payment途中だよ

---
+++ STYLICO/resources/views/payment.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <h2>STYLICO</h2>
+    <h2>Payment</h2>
+
+    <p><h4>- お届け日時 -</h4></p>
+    <p> <button type = "radio">通常配送</button></p>
+    <p> <button type = "radio">日時配送</button></p>
+    <p> <button type = "radio">即日配送</button></p>
+
+    
+</body>
+</html>
