b1578e1664f66bffc255bd4df398d7bcf99a6fea
Tomokiyo-H <2301179@s.asojuku.ac.jp>
2024-11-19 12:32:54 +0900

    マイサイズの追加
    追加とそれによるコントローラーの変更

--- STYLICO/app/Http/Controllers/StylicoController.php
+++ STYLICO/app/Http/Controllers/StylicoController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Carbon\Carbon;
+use Illuminate\Support\Facades\Auth;
 
 class StylicoController extends Controller
 {
@@ -39,10 +40,6 @@ class StylicoController extends Controller
         return view('updateaccount');
     }
 
-    public function paymentPostView(){
-        return view('payment');
-    }
-
     public function datetimeView(){
         $dates =[];
         $starDate=Carbon::today();
@@ -52,5 +49,22 @@ class StylicoController extends Controller
         }
         return view('datetime');
     }
-    
+
+    public function edit()
+    {
+        $user = Auth::user();
+        return view('mysize', compact('user'));
+    }
+
+    public function update(Request $request)
+    {
+        $request->validate([
+            'height' => 'required|numeric|min:50|max:300',
+            'weight' => 'required|numeric|min:20|max:500',
+        ]);
+        $user = Auth::user();
+        $user->height = $request->input('height');
+        $user->weight = $request->input('weight');
+        return redirect()->route('mysize.edit')->with('success', '情報が更新されました！');
+    }
 }
---
+++ STYLICO/resources/views/mysize.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<h2>STYLICO</h2><br><br>
+<h2>Update My Size</h2><br><br>
+<div class="container">
+    <h3>-現在の登録情報-</h3>
+        <div class="section">
+            <div class="section-title">現在の登録情報</div>
+            <div class="info-item">
+                <span>身長：</span>
+                <span>{{ $user->height ?? '---' }}cm</span>
+            </div>
+            <div class="info-item">
+                <span>体重：</span>
+                <span>{{ $user->weight ?? '---' }}kg</span>
+            </div>
+            <div class="info-item">
+                <span>靴のサイズ：</span>
+                <span>{{ $user->shoe_size ?? '---' }}cm</span>
+            </div>
+            <div class="info-item">
+                <span>普段の洋服のサイズ：</span>
+                <span>{{ $user->clothing_size ?? '---' }}</span>
+            </div>
+        </div>
+    <h3>-登録情報変更-</h3>
+        <div class="section">
+            <div class="section-title">登録情報変更</div>
+            <form action="{{ route('mysize.update') }}" method="POST">
+                @csrf
+                <div class="form-group">
+                    <label for="height">身長：</label>
+                    <input type="number" id="height" name="height" value="{{ old('height', $user->height) }}" placeholder="例: 180">
+                </div>
+                <div class="form-group">
+                    <label for="weight">体重：</label>
+                    <input type="number" id="weight" name="weight" value="{{ old('weight', $user->weight) }}" placeholder="例: 70">
+                </div>
+                <div class="form-group">
+                    <label for="shoe_size">靴のサイズ：</label>
+                    <input type="number" id="shoe_size" name="shoe_size" value="{{ old('shoe_size', $user->shoe_size) }}" placeholder="例: 28">
+                </div>
+                <div class="form-group">
+                    <label for="clothing_size">普段の洋服のサイズ：</label>
+                    <input type="text" id="clothing_size" name="clothing_size" value="{{ old('clothing_size', $user->clothing_size) }}" placeholder="例: L">
+                </div>
+            </form>
+    <h3>上記の情報でマイサイズを変更します</h3>
+<input type="submit" value="確定"><br><br>
+</body>
