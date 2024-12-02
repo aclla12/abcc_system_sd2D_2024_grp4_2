4547408a26ac99dee67809c8aae74f430f96feba
k-itoring <2301186@s.asojuku.ac.jp>
2024-12-02 13:57:34 +0900

    CSS作成
    CSS作成

--- STYLICO/resources/views/login.blade.php
+++ STYLICO/resources/views/login.blade.php
@@ -15,9 +15,9 @@
     @csrf
 <h5 class="login_id">ログインID</h5>
 <input type = "text" class="login_id" name = "loginid" >
-<h5>パスワード</h5>
-<input type="text" name = "passid" ><br><br>
-<input type="submit" value="ログイン"><br><br>
+<h5 class="pass">パスワード</h5>
+<input type="text" class="pass" name = "passid" ><br><br>
+<input type="submit" class="login" value="ログイン"><br><br>
 <input type="checkbox">次回から自動ログインする<br><br><br>
 <a href="#">パスワードを忘れた方へ→</a>
 </form>
