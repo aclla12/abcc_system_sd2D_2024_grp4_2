9add9c166d8676b80aaa5602481625201a7128d1
k-itoring <2301186@s.asojuku.ac.jp>
2024-12-08 12:16:16 +0900

    CSS作成
    CSS作成

--- STYLICO/resources/views/cart.blade.php
+++ STYLICO/resources/views/cart.blade.php
@@ -13,7 +13,7 @@
 
     <form action="#" method = "post">
     <h2 class="logo">STYLICO</h2>
-    <h2>CART</h2>
+    <h2 class="title">CART</h2>
     <p><h6>商品番号</h6></p>
     <p><h6>データベースから商品名</h6></p>
     <p><h6>DBから色／サイズ</h6></p>
@@ -25,17 +25,23 @@
     <button type = "submit">支払いへ進む</button>
 </form>
 
-    MENU <form action={{route('home')}} method = "post">
+<div class="footer">
+MENU 
+<form action={{ route('home') }} method = "post">
     @csrf
-        <button type = "submit">🏠</button></form>
-            <form action={{route('mypage')}} method = "post">
-                @csrf
-                <button type = "submit">👤</button></form>
-                    <form action={{ route('cart') }} method = "post">
-                        @csrf
-                        <button type = "submit">🛒</button>
-                    </form>
-            </form>
-    </form>
+    <button type = "submit" class="home">🏠</button>
+</form>
+            
+<form action={{ route('mypage') }} method = "post">
+    @csrf
+    <button type = "submit">👤</button>
+</form>
+
+<form action={{ route('cart') }} method = "post">
+    @csrf
+    <button type = "submit">🛒</button>
+</form>
+</div>
+
 </body>
 </html>
